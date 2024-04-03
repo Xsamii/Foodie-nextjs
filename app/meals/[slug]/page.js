e6fef48c694f35cbac8106913c06b77967a25493
Xsamii <a.samii1250@gmail.com>
2024-04-03 04:46:30 +0200

    first commit with basic structure and assets of project

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,9 @@
+const MealsSlugPafe = () => {
+  return (
+    <main>
+      <h1>Meals Slug Page</h1>
+    </main>
+  );
+};
+
+export default MealsSlugPafe;
---
+++ app/meals/share/page.js
@@ -0,0 +1,9 @@
+const MealsSharePage = () => {
+  return (
+    <main>
+      <h1>Meals Share Page</h1>
+    </main>
+  );
+};
+
+export default MealsSharePage;
